e01641f6be96530a4da7d378bbe299bf19f2e246
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-04-23 23:55:21 +0530

    thank com

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import thank from '../assets/thank.png'
-import Linkcom from './Linkcom'
+import React from 'react';
+import thank from '../assets/thank.png';
+import Linkcom from './Linkcom';
+
 const Thankyou = () => {
   return (
     <>
-     <div style={{ marginBottom: '4%' }}> 
-      <img src={thank} alt="Holidays Banner" style={{ width: '100%', height: 'auto' }} />
-    </div>
-    <Linkcom children={"Thank you"}/>
-    <div className='d-flex justify-content-center'>
+      <div style={{ marginBottom: '4%' }}> 
+        <img src={thank} alt="Holidays Banner" style={{ width: '100%', height: 'auto' }} />
+      </div>
+      {/* Correct way to pass children to Linkcom */}
+      <Linkcom>Cruises</Linkcom>
+      <div className='d-flex justify-content-center'>
         <h1>Thank you for choosing GT Holidays</h1>
         <p>We have received your message regarding your trip enquiry. Our team will be in touch with you shortly.</p>
-
         <p>If you want to speak to us kindly call us at +91 9940 8822 00.</p>
-    </div>
-
-
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Thankyou
+export default Thankyou;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,7 +23,7 @@ const Home = () => {
       <Content/>
       <Familytour/>
       <Address/>
-      <Thankyou/>
+     
 
       
 

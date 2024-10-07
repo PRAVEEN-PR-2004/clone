a108c4d0dc895b4d80c0212c65e443be23a6fdff
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-10-07 11:54:59 +0530

    again start

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,46 +1,37 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from "react";
 
-import Banner from '../components/Banner'
-import TextCard from '../components/TextCard'
-import ImgBanner from '../components/imgbanner'
+import Banner from "../components/Banner";
+import TextCard from "../components/TextCard";
+import ImgBanner from "../components/imgbanner";
 
-import Spot from '../components/Spot'
-import Content from '../components/Content'
-import Address from '../components/Address'
-import Familytour from '../components/Familytour'
-import Footer from '../components/Footer'
-import Slider1 from '../components/Slider1'
-import Slider2 from '../components/Slider2';
-import Slider3 from '../components/Slider3';
-
-
+import Spot from "../components/Spot";
+import Content from "../components/Content";
+import Address from "../components/Address";
+import Familytour from "../components/Familytour";
+import Footer from "../components/Footer";
+import Slider1 from "../components/Slider1";
+import Slider2 from "../components/Slider2";
+import Slider3 from "../components/Slider3";
 
 const Home = () => {
   useEffect(() => {
-    window.scrollTo(0, 0); 
+    window.scrollTo(0, 0);
   }, []);
   return (
     <>
-     
-
-        <Banner/>
-        <Spot/>
-       <TextCard/>
-      <ImgBanner/>
-      <Content/>
-       <Slider1/>
-      <Familytour/>
-      <Slider2/>
-      <Slider3/>
-      <Address/>
-      <Footer/>
-     
-
-      
-
-       
+      <Banner />
+      <Spot />
+      <TextCard />
+      <ImgBanner />
+      <Content />
+      <Slider1 />
+      <Familytour />
+      <Slider2 />
+      <Slider3 />
+      <Address />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

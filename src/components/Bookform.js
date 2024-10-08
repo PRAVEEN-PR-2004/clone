4a05b4f3b5c1dac4607be55e1d634b2a6c387196
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-10-08 11:45:55 +0530

    tel

--- src/components/Bookform.js
+++ src/components/Bookform.js
@@ -1,35 +1,72 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import './Bookform.css'; // Import the CSS file for custom styles
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./Bookform.css"; // Import the CSS file for custom styles
 
 const Bookform = () => {
   return (
     <>
-    
-      <Form className="secondcolform mt-5 text-center with-shadow"  > {/* Add the "with-shadow" class */}
+      <Form className="secondcolform mt-5 text-center with-shadow">
+        {" "}
+        {/* Add the "with-shadow" class */}
         <br />
-        <p style={{width:"80%",fontSize:"14px",fontWeight:"700",color:"#808080"}}>Get the Guaranteed Best Vacation Deals from GT Holidays</p>
+        <p
+          style={{
+            width: "80%",
+            fontSize: "14px",
+            fontWeight: "700",
+            color: "#808080",
+          }}
+        >
+          Get the Guaranteed Best Vacation Deals from GT Holidays
+        </p>
         <Form.Group className="mb-4 gtform" controlId="formName">
-          <Form.Control className="formcontrol" type="text" placeholder="Name" />
+          <Form.Control
+            className="formcontrol"
+            type="text"
+            placeholder="Name"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formCity">
-          <Form.Control className="formcontrol" type="text" placeholder="City of Residence" />
+          <Form.Control
+            className="formcontrol"
+            type="text"
+            placeholder="City of Residence"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formEmail">
-          <Form.Control className="formcontrol" type="email" placeholder="Email" />
+          <Form.Control
+            className="formcontrol"
+            type="email"
+            placeholder="Email"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formPhone">
-          <Form.Control className="formcontrol" type="number" placeholder="Phone Number" />
+          <Form.Control
+            className="formcontrol"
+            type="tel"
+            placeholder="Phone Number"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formCity">
-          <Form.Control className="formcontrol" type="text" placeholder="Destination" />
+          <Form.Control
+            className="formcontrol"
+            type="text"
+            placeholder="Destination"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formDate">
-  <Form.Control className="formcontrol" type="date" placeholder="dd-mm-yyyy" />
-</Form.Group>
-
+          <Form.Control
+            className="formcontrol"
+            type="date"
+            placeholder="dd-mm-yyyy"
+          />
+        </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formPeople">
-          <Form.Control className="formcontrol" type="number" placeholder="No. of people" />
+          <Form.Control
+            className="formcontrol"
+            type="number"
+            placeholder="No. of people"
+          />
         </Form.Group>
         <Form.Group className="mb-4 gtform" controlId="formVacationType">
           <Form.Select aria-label="Vacation Type">
@@ -40,12 +77,11 @@ const Bookform = () => {
             <option value="4">Corporate</option>
           </Form.Select>
         </Form.Group>
-        <Button style={{ width: '60%' }} className="sbtn" type="submit">
+        <Button style={{ width: "60%" }} className="sbtn" type="submit">
           Submit form
         </Button>
         <br />
       </Form>
-     
     </>
   );
 };
